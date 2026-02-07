edb9f5a0c9a09fc3a597ebcaa649927c3fbade64
halliday2026 <chris@hallidayinc.com>
2026-02-06 23:00:16 -0800

    update for Droid PWA

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -20,6 +20,12 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any" as const,
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
         purpose: "maskable" as const,
       },
     ],
